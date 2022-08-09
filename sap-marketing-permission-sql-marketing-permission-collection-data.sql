ff176efff385ae387d827ea4ad389cf871d9c683
Yurino Oga <yurino.o@latona.io>
2022-08-09 09:56:24 +0900

    release

--- sap-marketing-permission-sql-marketing-permission-collection-data.sql
+++ sap-marketing-permission-sql-marketing-permission-collection-data.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `sap_marketing_permission_collection_date`
+CREATE TABLE `sap_marketing_permission_marketing_permission_collection_data`
 (
-            `ObjectID`            varchar(70)   NOT  NULL
-			`ETag`                varchar(80)   DEFAULTNULL,
-			`BusinessPartnerID`   varchar(80)   DEFAULTNULL,
-			`BusinessPartnerUUID` varchar(80)   DEFAULTNULL,
-			`CreationDateTime`    varchar(80)   DEFAULTNULL,
-			`LastChangeDateTime`  varchar(80)   DEFAULTNULL,
-			`AlternativeID`       varchar(40)   DEFAULTNULL,
-			`EntityLastChangedOn` varchar(80)   DEFAULTNULL,
-			PRIMARY KEY (`ObjectID`) 
-			) ENGINE = InnoDB
-			DEFAULT CHARSET = utf8mb4;
+	`ObjectID`            varchar(70)   NOT  NULL
+	`ETag`                varchar(80)   DEFAULT NULL,
+	`BusinessPartnerID`   varchar(80)   DEFAULT NULL,
+	`BusinessPartnerUUID` varchar(80)   DEFAULT NULL,
+	`CreationDateTime`    varchar(80)   DEFAULT NULL,
+	`LastChangeDateTime`  varchar(80)   DEFAULT NULL,
+	`AlternativeID`       varchar(40)   DEFAULT NULL,
+	`EntityLastChangedOn` varchar(80)   DEFAULT NULL,
+	PRIMARY KEY (`ObjectID`) 
+) ENGINE = InnoDB
+DEFAULT CHARSET = utf8mb4;
